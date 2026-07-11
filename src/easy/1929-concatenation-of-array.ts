e00cb5257af981fed20635925b85c42551684686
Vansh <vansh.tuteja@edu.sait.ca>
2026-07-10 23:25:29 -0600

    Add solution skeleton for LeetCode 1929

---
+++ src/easy/1929-concatenation-of-array.ts
@@ -0,0 +1,15 @@
+/**
+ * LeetCode 1929 - Concatenation of Array
+ *
+ * Difficulty: Easy
+ */
+
+function getConcatenation(nums: number[]): number[] {
+  const ans: number[] = [];
+
+  return ans;
+}
+
+// Test Cases
+console.log(getConcatenation([1, 2, 1]));
+console.log(getConcatenation([1, 3, 2, 1]));
